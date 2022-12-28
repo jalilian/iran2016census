e14dd8084ab4d1d1bacac1c40e152c023c2c2929
Abdollah Jalilian <stat4aj@gmail.com>
2022-12-28 18:55:31 +0330

    Update iran2011census.R

--- SCIdata/iran2011census.R
+++ SCIdata/iran2011census.R
@@ -96,6 +96,8 @@ popdata <- pdata %>%
 popdata <- popdata %>% 
   # fix county name spelling
   mutate(county_name=recode(county_name,
+                            "چاه بهار"="چابهار",
+                            "اسداباد"="اسدآباد",
                             "آران وبیدگل"="آران و بیدگل",
                             "اسلام آبادغرب"="اسلام آباد غرب",
                             "بو یین و میاندشت"="بوئین میاندشت",
